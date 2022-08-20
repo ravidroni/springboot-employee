5710ab3eb666f4e79f778b4247d4cfdba5802976
Sravan <ravidroni200@gmail.com>
2022-08-21 00:16:46 +0530

    Initial commit

---
+++ src/main/java/com/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.repository;
+
+import com.springboot.model.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee,Integer> {
+}
